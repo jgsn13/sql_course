3713e00f672bb8f555fe99ebfeddbb96c1a99b53
Joaquim Gregório <joaquim.ibi@gmail.com>
2021-12-04 12:58:17 -0300

    update: WHERE

---
+++ 03_where.sql
@@ -0,0 +1,20 @@
+-- WHERE
+-- Útil quando queremos extrair apenas alguns dados de uma tabela
+SELECT coluna1,coluna2,coluna_n
+FROM tabela
+WHERE condicao
+
+/*
+Para determinar uma condição, vamos usar os seguintes operadores:
+
+OPERADOR   -   DESCRIÇÂO
+=               Igual
+>               Maior que
+<               Menor que
+>=              Maior ou igual que
+<=              Menor ou igual que
+<>              Diferente de
+AND             E
+OR              Ou
+
+*/
